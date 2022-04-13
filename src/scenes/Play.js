395dcf5a153a7873dc173d197f5ebc6f1140b289
Jessica <jhuan221@ucsc.edu>
2022-04-13 16:00:54 -0700

    speedy (faster and smaller spaceship) is now working

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -27,25 +27,25 @@ class Play extends Phaser.Scene{
         0x00FF00).setOrigin(0,0);
         // white borders
         this.add.rectangle(0,0, game.config.width, borderUISize,
-        0xFFFFFF).setOrigin(0,0);
+        0x000000).setOrigin(0,0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width,
-        borderUISize, 0xFFFFFF).setOrigin(0,0);
+        borderUISize, 0x000000).setOrigin(0,0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 
-        0xFFFFFF).setOrigin(0,0);
+        0x000000).setOrigin(0,0);
         this.add.rectangle(game.config.width -borderUISize, 0, borderUISize,
-        game.config.height, 0xFFFFFF).setOrigin(0,0);
+        game.config.height, 0x000000).setOrigin(0,0);
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize -
         borderPadding, 'rocket').setOrigin(0.5, 0);
         // add spaceships (x3)
         this.ship01 = new Spaceship(this, game.config.width + 
-        borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+        borderUISize*6, borderUISize*5, 'spaceship', 0, 30).setOrigin(0, 0);
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3,
-        borderUISize*5 + borderPadding*2, 'spaceship', 0, 10).setOrigin(0,0);
-        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship',
+        borderUISize*6 + borderPadding*3, 'spaceship', 0, 10).setOrigin(0,0);
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*7 + borderPadding*5, 'spaceship',
         0, 10).setOrigin(0,0);
-        this.ship04 = new Spaceship(this, game.config.width + borderUISize*5, borderUISize*8 + borderPadding*9, 'speedy',
-        0, 10).setOrigin(0,0);
+        this.ship04 = new Spaceship(this, game.config.width - borderUISize, borderUISize*4, 'speedy',
+        0, 40).setOrigin(0,0);
         // define keys
         keyF  = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
@@ -130,6 +130,10 @@ class Play extends Phaser.Scene{
         if (this.checkCollision(this.p1Rocket, this.ship01)){
             this.p1Rocket.reset();
             this.shipExplode(this.ship01);
+        }
+        if(this.checkCollision(this.p1Rocket, this.ship04)){
+            this.p1Rocket.reset();
+            this.shipExplode(this.ship04);
         }
     }
 
